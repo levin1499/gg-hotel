687c62bcf8e4d3709997711661e0cbd211d94050
L3V!N <85253400+levin1499@users.noreply.github.com>
2023-09-15 03:48:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+/* home slider */
+
+var swiper = new Swiper(".home-slider", {
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+
+/* team section */
+
+let imgBx = document.querySelectorAll(".imgBx");
+let contentBx = document.querySelectorAll(".contentBx");
+
+for (let i = 0; i < imgBx.length; i++) {
+  imgBx[i].addEventListener("mouseover", function () {
+    for (let i = 0; i < contentBx.length; i++) {
+      contentBx[i].className = "contentBx";
+    }
+    document.getElementById(this.dataset.id).className = "contentBx active";
+
+    for (let i = 0; i < imgBx.length; i++) {
+      imgBx[i].className = "imgBx";
+    }
+    this.className = "imgBx active";
+  });
+}
